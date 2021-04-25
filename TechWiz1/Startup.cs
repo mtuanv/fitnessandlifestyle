e0653ab279a401ba88108c53c8140433232fdeb6
DucLe1109 <74303018+DucLe1109@users.noreply.github.com>
2021-04-25 09:05:23 +0700

    ádad
    adas

---
+++ TechWiz1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TechWiz1.Startup))]
+namespace TechWiz1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
