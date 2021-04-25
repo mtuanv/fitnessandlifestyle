1b73871161ec234f6e52015c4170245e86c1e8ea
DucLe1109 <74303018+DucLe1109@users.noreply.github.com>
2021-04-25 20:43:37 +0700

    BMI
    tính chỉ số BMI

--- fitness/Controllers/HomeController.cs
+++ fitness/Controllers/HomeController.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using fitness.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 
@@ -8,14 +10,61 @@ namespace fitness.Controllers
 {
     public class HomeController : Controller
     {
+        fitnessandlifestyle db = new fitnessandlifestyle();
         public ActionResult Index()
         {
             return View();
         }
-         public ActionResult Team()
+        public ActionResult Team()
         {
             return View();
         }
+        public ActionResult BMI()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public JsonResult calculateBMI(BMI bmi)
+        {
+            if (bmi.Height != 0)
+            {
+                float weight = bmi.Weight;
+                float height = (float)(bmi.Height)/100;
+                float res = (weight / (height * height));
+                string result = String.Format("{0:0.00}", res);
+                return Json(result, JsonRequestBehavior.AllowGet);
+            }
+            else
+            {
+                return Json("Error", JsonRequestBehavior.AllowGet);
+            }
+        }
+        public JsonResult listWorkOut(BMI_parameter bMI_Parameter)
+        {
+            
+            bool proxyCreation = db.Configuration.ProxyCreationEnabled;
+            try
+            {
+                //set ProxyCreation to false
+                db.Configuration.ProxyCreationEnabled = false;
+
+                var data = db.WorkOuts.Where(i => i.Category == 2).ToList();
+
+                return Json(data, JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception ex)
+            {
+                Response.StatusCode = (int)HttpStatusCode.BadRequest;
+                return Json(ex.Message);
+            }
+            finally
+            {
+                //restore ProxyCreation to its original state
+                db.Configuration.ProxyCreationEnabled = proxyCreation;
+            }
+        }
+
 
         public ActionResult About()
         {
