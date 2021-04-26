22689d217e9c6f381226d93d51f5a9a29bf59195
Cao Vuong Bach <58335662+cvb1994@users.noreply.github.com>
2021-04-27 01:13:25 +0700

    update workout detail

--- fitness/Public/js/bach.js
+++ fitness/Public/js/bach.js
@@ -1,3 +1,4 @@
+
 function calBMI() {
     let weightInput = $("#weight").val();
     let heightInput = $("#height").val();
@@ -40,4 +41,74 @@ function moveArrow(bmi){
     } else {
         $(".bmi-container span").css("padding-left",(bmi-14)*ratio+"px");
     }
+}
+
+function disposeModal() {
+    var modalClose = $("#closeModal");
+    if (modalClose != null) {
+        $("#exampleModal").remove();
+    }
+}
+
+function loadModal(link, titleExercise) {
+    let container = document.getElementById("loadSection");
+    let modal = document.createElement("div");
+    modal.setAttribute("class", "modal fade show");
+    modal.setAttribute("id", "exampleModal");
+    modal.setAttribute("tabindex", "-1");
+    modal.setAttribute("aria-labelledby", "exampleModalLabel");
+    modal.setAttribute("aria-hidden", "true");
+    modal.setAttribute("style", "display: block; padding-right: 16px");
+    modal.setAttribute("role", "dialog");
+
+
+    let modalDialog = document.createElement("div");
+    modalDialog.setAttribute("class", "modal-dialog modal-dialog-centered");
+
+    let modalContent = document.createElement("div");
+    modalContent.setAttribute("class", "modal-content");
+
+    let modalHeader = document.createElement("div");
+    modalHeader.setAttribute("class", "modal-header");
+
+    let title = document.createElement("h5");
+    title.setAttribute("class", "modal-title");
+    title.setAttribute("id", "exampleModalLabel");
+    title.innerHTML = titleExercise;
+
+    let modalBody = document.createElement("div");
+    modalBody.setAttribute("class", "modal-body");
+
+    let frame = document.createElement("iframe");
+    frame.setAttribute("width", "470");
+    frame.setAttribute("height", "315");
+    frame.setAttribute("src", "https://www.youtube.com/embed/" + link);
+    frame.setAttribute("frameborder", "0");
+    frame.setAttribute("allow", "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture");
+
+    /*let content = document.createElement("div");
+    content.setAttribute("class", "content-video");
+    content.innerHTML = content_clip;*/
+
+    let modalFooter = document.createElement("div");
+    modalFooter.setAttribute("class", "modal-footer");
+
+    let close = document.createElement('button');
+    close.setAttribute("type", "button");
+    close.setAttribute("class", "btn btn-secondary");
+    close.setAttribute("data-bs-dismiss", "modal");
+    close.setAttribute("id", "closeModal");
+    close.setAttribute("onclick", "disposeModal()");
+    close.innerHTML = "Close";
+
+    modalHeader.appendChild(title);
+    modalFooter.appendChild(close);
+    modalBody.appendChild(frame);
+    modalContent.appendChild(modalHeader);
+    modalContent.appendChild(modalBody);
+    //modalContent.appendChild(content);
+    modalContent.appendChild(modalFooter);
+    modalDialog.appendChild(modalContent);
+    modal.appendChild(modalDialog);
+    container.appendChild(modal);
 }
