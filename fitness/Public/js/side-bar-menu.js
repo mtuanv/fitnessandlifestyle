7ff5780a7489bb1c9a5e585c911a9b5a0ebade94
Dương Hoàng <manchestervn1996@gmail.com>
2021-04-27 18:52:21 +0700

    delete folder

--- fitness/Public/js/side-bar-menu.js
+++
@@ -1,8 +0,0 @@
-$(function () {
-    var path = window.location.href;
-    $('.about .nav-link').each(function () {
-        if (this.href === path) {
-            $(this).addClass('active_sidebar');
-        }
-    });
-})
