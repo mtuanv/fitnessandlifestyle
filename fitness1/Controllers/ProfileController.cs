56320d72ca42388a3a6ebe1c549beab3046dac5f
Cao Vuong Bach <58335662+cvb1994@users.noreply.github.com>
2021-04-27 21:59:01 +0700

    update

--- fitness1/Controllers/ProfileController.cs
+++ fitness1/Controllers/ProfileController.cs
@@ -13,16 +13,37 @@ namespace fitness.Controllers
         // GET: Profile
         public ActionResult UserDiagram()
         {
-            return View();
+            var username = System.Web.HttpContext.Current.User.Identity.Name;
+            AspNetUser user = db.AspNetUsers.Where(u => u.Email.Equals(username)).First();
+            Goal goal = db.Goals.Where(n => n.UserId == user.Id).FirstOrDefault();
+            if (goal != null)
+            {
+                string listProgess = "";
+                ViewBag.GoalID = goal.Id;
+                var list = db.GoalProgesses.Where(n => n.GoalId == goal.Id).ToList();
+                for(int i = 0; i < list.Count(); i++)
+                {
+                    listProgess += list[i].timestamp + "-" + list[i].CurrentWeight + "--";
+                }
+                ViewBag.progess = listProgess;
+            }
+            return View(user);
         }
-
+        [Authorize]
         public ActionResult UserProfile()
         {
             var username = System.Web.HttpContext.Current.User.Identity.Name;
             AspNetUser user = db.AspNetUsers.Where(u => u.Email.Equals(username)).First();
+
+            //Goal goal = db.Goals.Find("UserId = '"+ user.Id + "'");
+            Goal goal = db.Goals.Where(n => n.UserId == user.Id).FirstOrDefault();
+            if(goal != null)
+            {
+                ViewBag.Goal = goal.WeightDesired;
+            }
             return View(user);
         }
-
+        [Authorize]
         public ActionResult UserPlan()
         {
             var username = System.Web.HttpContext.Current.User.Identity.Name;
@@ -30,11 +51,12 @@ namespace fitness.Controllers
             var listOrder = db.Orders.Where(o => o.UserId == user.Id && o.timestamp != null).ToList();
             return View(listOrder);
         }
-
+        [Authorize]
         public ActionResult UserBMI()
         {
             return View();
         }
+        [Authorize]
         public ActionResult UserOrder()
         {
             var username = System.Web.HttpContext.Current.User.Identity.Name;
@@ -42,6 +64,7 @@ namespace fitness.Controllers
             var listOrder = db.Orders.Where(o => o.UserId == user.Id && o.timestamp == null).ToList();
             return View(listOrder);
         }
+        [Authorize]
         [HttpPost]
         [ValidateAntiForgeryToken]
         public ActionResult UpdateProfile(string firstname, string lastname, int gender, int age, int weight, int height, int desire_weight )
@@ -66,50 +89,72 @@ namespace fitness.Controllers
                 category = 2;
             }
 
-            Goal goal = new Goal();
-            goal.WeightDesired = desire_weight;
-            goal.UserId = user.Id;
-            goal.Category = category;
-            goal.StatusGoal = 0;
-            db.Goals.Add(goal);
-            db.SaveChanges();
+            Goal goal = db.Goals.Where(n => n.UserId == user.Id).FirstOrDefault();
+            if(goal != null)
+            {
+                goal.WeightDesired = desire_weight;
+                goal.UserId = user.Id;
+                goal.Category = category;
+                goal.StatusGoal = 0;
+                db.SaveChanges();
+            } else
+            {
+                Goal newGoal = new Goal();
+                newGoal.WeightDesired = desire_weight;
+                newGoal.UserId = user.Id;
+                newGoal.Category = category;
+                newGoal.StatusGoal = 0;
+                db.Goals.Add(newGoal);
+                db.SaveChanges();
+            }
+            
             return RedirectToAction("Index", "Home");
         }
 
-
+        [HttpPost]
         /*/Profile/GetListProgress*/
-        public JsonResult GetListProgress(string searchTerm)
+        public JsonResult GetListProgress(int id)
         {
             db.Configuration.ProxyCreationEnabled = false;
-            var list = db.GoalProgesses.ToList();
+            //var list = db.GoalProgesses.ToList();
+            var list = db.GoalProgesses.Where(n => n.GoalId == id).ToList();
+            //var list = from progess in db.GoalProgesses where progess.GoalId == userGoal.Id select progess;
             return Json(list, JsonRequestBehavior.AllowGet);
         }
-
+        [Authorize]
         public ActionResult UserInputInfo()
         {
             var username = System.Web.HttpContext.Current.User.Identity.Name;
             AspNetUser user = db.AspNetUsers.Where(u => u.Email.Equals(username)).First();
             return View(user);
         }
-
+        [Authorize]
         [HttpPost]
         public ActionResult UpdateProgess(int weight, int height)
         {
+
             var username = System.Web.HttpContext.Current.User.Identity.Name;
             AspNetUser user = db.AspNetUsers.Where(u => u.Email.Equals(username)).First();
 
-            Goal userGoal = db.Goals.Find("UserId = " + user.Id);
-            int goalId = userGoal.Id;
+            Goal userGoal = db.Goals.Where(n => n.UserId == user.Id).FirstOrDefault();
+            if (userGoal != null)
+            {
+                int goalId = userGoal.Id;
 
-            GoalProgess progess = new GoalProgess();
-            progess.CurrentHeight = height;
-            progess.CurrentWeight = weight;
-            progess.GoalId = goalId;
-            progess.timestamp = DateTime.Now;
-            db.GoalProgesses.Add(progess);
-            db.SaveChanges();
+                GoalProgess progess = new GoalProgess();
+                progess.CurrentHeight = height;
+                progess.CurrentWeight = weight;
+                progess.GoalId = goalId;
+                progess.timestamp = DateTime.Now;
+                db.GoalProgesses.Add(progess);
+                db.SaveChanges();
 
-            return RedirectToAction("UserPlan", "Profile");
+                return RedirectToAction("UserPlan", "Profile");
+            } else
+            {
+                return RedirectToAction("UserProfile", "Profile");
+            }
+            
         }
     }
 }
