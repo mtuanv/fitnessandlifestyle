f7e63e508dc1d14ce8cc2b547ccf59d6478efb1a
DucLe1109 <74303018+DucLe1109@users.noreply.github.com>
2021-04-27 15:51:22 +0700

    abc

---
+++ fitness1/Models/WorkOut_Join_Order.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace fitness.Models
+{
+    public class WorkOut_Join_Order
+    {
+        public int Id { get; set; }
+        public string title { get; set; }
+      
+    }
+}
